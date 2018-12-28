cfff3b970a6c70641425c6c162349febe75e6a1e
Markus Maga <markus@maga.se>
2018-12-28 17:06:58 +0100

    🚧

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import React from "react";
+import { useTrail, animated } from "react-spring/hooks";
+import "./App.css";
+const config = { mass: 5, tension: 2000, friction: 200 }
+const App = () => {
+  const items = ["M", "a", "r", "k", "u", "s", "\u00a0", "M", "a", "g", "a"];
+  const trail = useTrail(items.length, {
+    config,
+    opacity: 1,
+    x: 0,
+    height: 80,
+    from: { opacity: 0, x: 20, height: 0 },
+  })
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span className="App-logo" role="img">
+          🤪
+        </span>
+        <div className="App-name">
+        {trail.map(({ x, height, ...rest }, index) => (
+          <animated.span key={rest.key}
+            style={{ ...rest, transform: x.interpolate(x => `translate3d(0,${x}px,0)`) }}>
+            {items[index]}
+          </animated.span>
+        ))}
+        </div>
+      </header>
+    </div>
+  );
+};
 
 export default App;
