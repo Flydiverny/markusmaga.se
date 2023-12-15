3bede969bcd4d6d58d57462e39fbd1462369899a
Markus Maga <markus@maga.se>
2023-12-15 16:25:42 +0100

    fix: animate out on click, auto reset
    Signed-off-by: Markus Maga <markus@maga.se>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,32 +7,33 @@ import { Pixelify_Sans } from "next/font/google"
 // import { Patua_One } from 'next/font/google'
 import { Mate } from "next/font/google"
 import "./globals.css"
+import { useTimeout } from "usehooks-ts"
 
 const inter = Pixelify_Sans({ subsets: ["latin"], weight: ["400", "700"] })
 const mateIt = Mate({ subsets: ["latin"], style: "normal", weight: ["400"] })
 
 const items = ["M", "a", "r", "k", "u", "s", "\u00a0", "M", "a", "g", "a"]
 
-const Name = ({
-  onClick,
-}: {
-  onClick: React.MouseEventHandler<HTMLHeadElement>
-}) => {
+const Name = () => {
   const [open, set] = useState(false)
   const trails = useTrail(items.length, {
     from: { opacity: 0, x: 20, y: 40},
     opacity: open ? 1 : 0,
     x: open ? 0 : 20,
     y: open ? 0 : 80,
-    config: { mass: 8, tension: 3500, friction: 200 },
+    config: { mass: open ? 8 : 4, tension: 3500, friction: open ? 200 : 200},
   })
 
   useEffect(() => {
     set(true)
   }, [])
 
+  useTimeout(() => {
+    set(true)
+  }, open ? null : 400)
+
   return (
-    <h1 className={inter.className} onClick={onClick}>
+    <h1 className={inter.className} onClick={() => set(!open)}>
       {trails.map((style, index) => (
         <a.span key={index} style={{ ...style, transform: style.y.to(y => `translate3d(0,${y}px,0)`)}}>
           {items[index]}
@@ -43,16 +44,10 @@ const Name = ({
 }
 
 const Header = () => {
-  const [key, set] = useState(Math.random())
 
   return (
     <div className={"wrapper"}>
-      <Name
-        key={key}
-        onClick={() => {
-          set(Math.random())
-        }}
-      />
+      <Name />
       <div className={`links ${mateIt.className}`}>
         <div className="link">
           <a href="https://github.com/Flydiverny">github</a>
