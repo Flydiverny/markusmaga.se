304b3237ab2a3011fa22e9280ad65d374a488591
Markus Maga <markus@maga.se>
2023-12-15 16:16:18 +0100

    fix: update animation and allow restarting it on click
    Signed-off-by: Markus Maga <markus@maga.se>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,14 +13,18 @@ const mateIt = Mate({ subsets: ["latin"], style: "normal", weight: ["400"] })
 
 const items = ["M", "a", "r", "k", "u", "s", "\u00a0", "M", "a", "g", "a"]
 
-const Header = () => {
+const Name = ({
+  onClick,
+}: {
+  onClick: React.MouseEventHandler<HTMLHeadElement>
+}) => {
   const [open, set] = useState(false)
   const trails = useTrail(items.length, {
-    from: { opacity: 0, x: 20 },
+    from: { opacity: 0, x: 20, y: 40},
     opacity: open ? 1 : 0,
     x: open ? 0 : 20,
-    // height: open ? 80 : 80,
-    config: { mass: 5, tension: 2000, friction: 200 },
+    y: open ? 0 : 80,
+    config: { mass: 8, tension: 3500, friction: 200 },
   })
 
   useEffect(() => {
@@ -28,14 +32,27 @@ const Header = () => {
   }, [])
 
   return (
+    <h1 className={inter.className} onClick={onClick}>
+      {trails.map((style, index) => (
+        <a.span key={index} style={{ ...style, transform: style.y.to(y => `translate3d(0,${y}px,0)`)}}>
+          {items[index]}
+        </a.span>
+      ))}
+    </h1>
+  )
+}
+
+const Header = () => {
+  const [key, set] = useState(Math.random())
+
+  return (
     <div className={"wrapper"}>
-      <h1 className={inter.className}>
-        {trails.map((style, index) => (
-          <a.span key={index} style={style}>
-            {items[index]}
-          </a.span>
-        ))}
-      </h1>
+      <Name
+        key={key}
+        onClick={() => {
+          set(Math.random())
+        }}
+      />
       <div className={`links ${mateIt.className}`}>
         <div className="link">
           <a href="https://github.com/Flydiverny">github</a>
